1511b9d7c6883542973b6875e61c6ec942f2b385
AnAs Abu Shammala <anasbasilsha@gmail.com>
2025-04-02 15:32:19 +0200

    login form

---
+++ WebApplication2/Controllers/AcountController.cs
@@ -0,0 +1,36 @@
+﻿using DataValidationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class AcountController : Controller
+    {
+        // GET: /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserLogin model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("LoginSuccess");
+        }
+
+        // GET: /Account/LoginSuccess
+        [HttpGet]
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
+
+    }
+}
